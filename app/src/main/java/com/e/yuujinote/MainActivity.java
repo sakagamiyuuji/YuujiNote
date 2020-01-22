8ce177fc1ccc1fc7a3294cc20b68cac843f47ac6
Sakagami Yuuji <varian.khasira@gmail.com>
2020-01-22 16:48:20 +0700

    FINALLY FINISHED

--- app/src/main/java/com/e/yuujinote/MainActivity.java
+++ app/src/main/java/com/e/yuujinote/MainActivity.java
@@ -5,17 +5,16 @@ import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 ;
@@ -25,9 +24,6 @@ public class MainActivity extends AppCompatActivity{
     private ListView lvItems;
     private ArrayList<String> items;
     private ArrayAdapter<String> itemsAdapter;
-    private EditText edtTitle, edtActivity;
-    private String title, activity;
-
     private final String NOTE_SP = "NOTE_SP";
 
     @Override
@@ -36,7 +32,7 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
         initialComponent();
 
-        loadData();
+        //loadData();
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,23 +41,43 @@ public class MainActivity extends AppCompatActivity{
             }
         });
 
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                final int which_item = position;
+
+                new AlertDialog.Builder(MainActivity.this)
+                .setIcon(R.drawable.ic_delete_forever_black_24dp)
+                .setTitle("DELETE FOREVER")
+                .setMessage("Are You Sure?")
+                .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        items.remove(which_item);
+                        sortSP();
+
+                        itemsAdapter.notifyDataSetChanged();
+
+                    }
+                })
+
+                .setNegativeButton("NO", null)
+                .show();
+
+                return true;
+            }
+        });
+
         items = new ArrayList<String>();
+        showSP();
         itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
         lvItems.setAdapter(itemsAdapter);
+
 
         /*for (int i = 0; i < 25; i++) {
             items.add("Item ke - " + i);
         }*/
-
-        save.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                saveData();
-            }
-        });
-
     }
-
 
     public void addtask () {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -72,8 +88,16 @@ public class MainActivity extends AppCompatActivity{
         builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
+                int new_key = items.size();
+                String item = inputField.getText().toString();
 
-                items.add(inputField.getText().toString());
+                items.add(new_key,item);
+                addToSh(new_key, item);
+
+                itemsAdapter.notifyDataSetChanged();
+
+                String text = String.valueOf(items.size());
+                Toast.makeText(getApplicationContext(),"DATA TELAH DI TAMBAHKAN", Toast.LENGTH_SHORT).show();
 
             }
         });
@@ -83,6 +107,43 @@ public class MainActivity extends AppCompatActivity{
         builder.create().show();
     }
 
+    private void delSP(int position){
+        String key = String.valueOf(position);
+        SharedPreferences sh = getSharedPreferences("todo", MODE_PRIVATE);
+        sh.edit().remove(key);
+        //hapus 1 item. celah kosong
+    }
+
+    private void sortSP(){
+        SharedPreferences sp = getSharedPreferences("todo",MODE_PRIVATE);
+        SharedPreferences.Editor editor= sp.edit();
+        editor.clear();
+        editor.apply();
+        for(int i = 0; i < items.size();i++){
+            editor.putString(String.valueOf(i),items.get(i));
+        }
+        editor.apply();
+
+    }
+
+    private void showSP(){
+        SharedPreferences sh = getSharedPreferences("todo", MODE_PRIVATE);
+        if (sh.getAll().size() > 0){
+            for (int i=0 ; i< sh.getAll().size(); i++){
+                String key = String.valueOf(i);
+                items.add(sh.getString(key, null));
+            }
+        }
+    }
+
+    private void addToSh(int key, String item){
+        SharedPreferences sh = getSharedPreferences("todo", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sh.edit();
+        String k = String.valueOf(key);
+        editor.putString(k, item);
+        editor.apply();
+    }
+
     public void initialComponent() {
         fab = findViewById(R.id.fab_btn);
         save = findViewById(R.id.save_btn);
@@ -90,6 +151,7 @@ public class MainActivity extends AppCompatActivity{
 
     }
 
+/*
     public void saveData(){
         SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
         //SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
@@ -106,42 +168,17 @@ public class MainActivity extends AppCompatActivity{
         Gson gson = new Gson();
         String json = sharedPreferences.getString("task list", null);
         Type type = new TypeToken<ArrayList<String>>() {}.getType();
-        items =  gson.fromJson(json, type);
+        items = gson.fromJson(json, type);
+
+        //Type type = new TypeToken<ArrayList<String>>() {}.getType();
+        //items =  gson.fromJson(json, type);
 
         if (items == null){
             items = new ArrayList<>();
         }
     }
+*/
 
-/*    @Override
-    protected void onPause() {
-        super.onPause();
 
-        SharedPreferences sp = getSharedPreferences(NOTE_SP, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sp.edit();
-
-        for (int i=0; i<= items.size(); i++){
-            String key = String.valueOf(i);
-            String item = items.get(i);
-            editor.putString(key,item);
-        }
-
-        editor.apply();
-    }
-
-    protected void showSP(){
-        SharedPreferences sp = getSharedPreferences(NOTE_SP, Context.MODE_PRIVATE);
-        int createCookies = 0;
-
-        for (int a = 0; a<= items.size(); a++){
-            String key = String.valueOf(a);
-            String cookies = sp.getString(key, null);
-            items.add(cookies);
-
-            if (items == null){
-                items = new ArrayList<>();
-            }
-        }
-    }*/
 }
 
